88f1266415b7d0ef9a28b664f9261da7a93e5682
nox1d <114980767+nox1d@users.noreply.github.com>
2025-05-20 10:08:41 +0800

    Add walk n idle animation to person

--- scripts/person.gd
+++ scripts/person.gd
@@ -1,34 +1,54 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 10.0
+const SPEED : float = 10.0
+
+@export var move_speed : float = SPEED
 @export var animation_tree : AnimationTree
 
-var direction: Vector2
-var wander_time: float
+var direction : Vector2
+var wander_time : float
+var thinking : bool = false
+var last_facing_direction := Vector2(0, -1)
 
-func randomize_wander():	
+func randomize_wander():
+	thinking = false
 	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
 	direction = direction.normalized()
-	print(direction)
 	wander_time = randf_range(0, 10)
 
 func _ready():
+	animation_tree.active = true
 	call_deferred("randomize_wander")
 
 func _process(delta):
+	if wander_time < 3:
+		thinking = true
+
 	if wander_time > 0:
 		wander_time -= delta
 	else:
-		
 		randomize_wander()
 
 func _physics_process(delta):
+	if thinking:
+		move_speed = 0.0
+	else:
+		move_speed = SPEED
+
 	velocity = direction * move_speed
 	
+	var idle = !velocity
+	if !idle:
+		last_facing_direction = velocity.normalized()
+		print(last_facing_direction)
+	
 	if velocity == Vector2.ZERO:
-		pass
+		animation_tree.get("parameters/playback").travel("Idle")
 	else:
-		animation_tree.set("parameters/Walk/blend_position", velocity.normalized())
-
-	move_and_slide()
+		animation_tree.get("parameters/playback").travel("Walk")
+		
+		animation_tree.set("parameters/Walk/blend_position", last_facing_direction)
+		animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
+		print("Idle:", idle, " Walk:", !idle)
+		move_and_slide()
 	
