46517c1ed44c4e962af23eaf9e138bc4c86b125e
nox1d <114980767+nox1d@users.noreply.github.com>
2025-05-20 20:01:30 +0800

    Add attack component

---
+++ scripts/attack.gd
@@ -0,0 +1,20 @@
+extends Node2D
+class_name Attack
+
+@export var attack_damage := 10.0
+@export var cooldown_time := 2.0
+
+var last_attack_time : float
+
+func _process(delta):
+	attack()
+
+func can_attack():
+	return (Time.get_ticks_msec() / 1000) - last_attack_time >= cooldown_time
+
+func attack():
+	if not can_attack():
+		return
+
+	last_attack_time = Time.get_ticks_msec() / 1000
+	print("attacking, " + str(last_attack_time))
