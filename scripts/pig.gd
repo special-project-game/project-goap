da3056a54836d1435ff70c71456ee79b88c80f7e
nox1d <114980767+nox1d@users.noreply.github.com>
2025-05-26 15:14:54 +0800

    Add state machine and wander state to pig, remove pig walk code, disable collision for pig

--- scripts/pig.gd
+++ scripts/pig.gd
@@ -6,47 +6,6 @@ const SPEED : float = 10.0
 @export var animation_tree : AnimationTree
 @onready var health_component = $HealthComponent
 
-var direction : Vector2
-var wander_time : float
-var thinking : bool = false
-var last_facing_direction := Vector2(0, -1)
-
-func randomize_wander():
-	thinking = false
-	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
-	direction = direction.normalized()
-	wander_time = randf_range(0, 10)
-	print("Pig Health: " + str(health_component.health))
-
-func _ready():
-	animation_tree.active = true
-	call_deferred("randomize_wander")
-
-func _process(delta):
-	if wander_time < 3:
-		thinking = true
-
-	if wander_time > 0:
-		wander_time -= delta
-	else:
-		randomize_wander()
-
 func _physics_process(delta):
-	if thinking:
-		move_speed = 0.0
-	else:
-		move_speed = SPEED
-
-	velocity = direction * move_speed
-	
-	var idle = !velocity
-	if !idle:
-		last_facing_direction = velocity.normalized()
-
-	
-	if velocity == Vector2.ZERO:
-		pass
-	else:
-		animation_tree.set("parameters/Walk/blend_position", last_facing_direction.x)
-		move_and_slide()
+	move_and_slide()
 	
