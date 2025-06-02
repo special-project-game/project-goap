597dceab6e40741657f5907380946c744d61bae5
nox1d <114980767+nox1d@users.noreply.github.com>
2025-06-02 16:44:19 +0800

    Move test_scan_tree() from person to find_tree, comment person astar script temporarily, add simple tree lookup pathfinding

--- scripts/find_tree.gd
+++ scripts/find_tree.gd
@@ -1,14 +1,50 @@
 extends State
 
 
+# test code for FindTree Action, will be moved to FindTree State
+@onready var navigation_agent_2d = $"../../NavigationAgent2D"
+@onready var scanner_component = $"../../ScannerComponent"
+@onready var person = $"../.."
+
+var nearby_trees : Array = []
+var target : Node2D = null
+
 func scan_trees():
-	pass
+	var tree_distances : Array
+	var self_position = person.global_position
+	nearby_trees = scanner_component.get_overlapping_bodies()
+	for tree in nearby_trees:
+		var tree_position = tree.global_position
+		tree_distances.append([tree, tree_position.distance_to(self_position)])
+	tree_distances.sort_custom(func(a, b): return a[1] < b[1])
+	if tree_distances.size() > 0:
+		var closest_tree = tree_distances[0]
+		print("closest tree: " + str(closest_tree))
+	
+	target = tree_distances[0][0]
+
+func setup_target():
+	await get_tree().physics_frame
+	scan_trees()
+	print(target)
+	
+	if is_instance_valid(target):
+		navigation_agent_2d.target_position = target.global_position
+		print(navigation_agent_2d.target_position)
 
 func Enter():
-	pass
+	call_deferred("setup_target")
 
 func Update(delta):
 	pass
 
 func Physics_Update(delta):
-	pass
+	if is_instance_valid(target):
+		navigation_agent_2d.target_position = target.global_position
+	if navigation_agent_2d.is_navigation_finished():
+		return
+
+	var current_agent_position = person.global_position
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+	
+	person.velocity = current_agent_position.direction_to(next_path_position) * person.move_speed
